3c67692783d1e8d11814c4418a79bab2047d7054
hyungkiShin <tkaqkeldk@nate.com>
2022-07-16 16:07:29 +0900

    feat 트랜잭션 두번사용

--- src/test/java/com/transaction/propagation/BasicTxTest.java
+++ src/test/java/com/transaction/propagation/BasicTxTest.java
@@ -52,4 +52,49 @@ class BasicTxTest {
         txManager.rollback(status);
         log.info("트랜잭션 롤백 완료");
     }
+
+    @Test
+    void double_commit() {
+
+        /*
+            Connection pull 을 같은걸 쓴다.
+            다른걸 쓸수 있는 방법은 없을까 ? 라고 생각했는데
+
+            사실 달랐다 ㅎ
+            같은걸 사용 했다고 하는 이유는 커넥션이 같아서 인데.
+            hikari connection pool 에서 커넥션을 획득하면, 실제 커넥션을 그대로 반환하는게 아니라
+            내부 관리를 위해 hikari proxy connection 이라는 객체를 생성해서 반환한다.
+            물론 내부에선 실제 커넥션이 포함되어 있다. 이 객체의 주소를 확인하면 커넥션 풀에서 획득한 커넥션을 구분할 수 있다.
+            결론. -> 커넥션만 같은걸 사용했지 ( 반납하면 다시 그걸써 ) 객체는 달랐다. :)
+         */
+
+        log.info("트랜잭션1 시작");
+        final TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        final TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(tx2);
+    }
+
+    @Test
+    void commit_and_rollback() {
+        log.info("트랜잭션1 시작");
+        final TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션 1 록백 시작");
+        final TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 1 록백");
+        txManager.rollback(tx2);
+
+
+    }
 }
